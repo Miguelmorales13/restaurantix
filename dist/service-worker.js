c8610ede4bb6f8d790f064d491f92a92f23099e6
miguel <miguel.moralesr@hotmail.com>
2021-05-22 12:50:55 -0500

    deploy

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.282321618ba96aebd6d779bbaa18a813.js"
+  "/restaurantix/precache-manifest.7ca3f6c601d2385952f0b312a19bdb83.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "restaurantix-vue"});
