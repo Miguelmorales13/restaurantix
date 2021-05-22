4263fcb93204f70ee6cd68e49ae4fddce1ce3e9a
miguel <miguel.moralesr@hotmail.com>
2021-05-22 12:50:55 -0500

    deploy

---
+++ precache-manifest.7ca3f6c601d2385952f0b312a19bdb83.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc7a079ae240bfdb10e6",
+    "url": "/restaurantix/js/Chefs.d447d669.js"
+  },
+  {
+    "revision": "7238202c48cf374f3f89",
+    "url": "/restaurantix/js/Chefs~Waiters~admin.9f1920b8.js"
+  },
+  {
+    "revision": "ec6a8f52b8ee697e98ba",
+    "url": "/restaurantix/css/Login.89dbc8b5.css"
+  },
+  {
+    "revision": "ec6a8f52b8ee697e98ba",
+    "url": "/restaurantix/js/Login.fe1460b3.js"
+  },
+  {
+    "revision": "7bee71ecd358501b1f29",
+    "url": "/restaurantix/css/Waiters.4909f206.css"
+  },
+  {
+    "revision": "7bee71ecd358501b1f29",
+    "url": "/restaurantix/js/Waiters.a4b1b296.js"
+  },
+  {
+    "revision": "fb06d9c556a5911f33b4",
+    "url": "/restaurantix/css/admin.64be0969.css"
+  },
+  {
+    "revision": "fb06d9c556a5911f33b4",
+    "url": "/restaurantix/js/admin.86de6c61.js"
+  },
+  {
+    "revision": "dae54a5116297a61bf75",
+    "url": "/restaurantix/css/app.70f830fa.css"
+  },
+  {
+    "revision": "dae54a5116297a61bf75",
+    "url": "/restaurantix/js/app.8f6ea345.js"
+  },
+  {
+    "revision": "6bbdd558241234310939",
+    "url": "/restaurantix/css/chunk-vendors.9a6be856.css"
+  },
+  {
+    "revision": "6bbdd558241234310939",
+    "url": "/restaurantix/js/chunk-vendors.c7d28755.js"
+  },
+  {
+    "revision": "a994321efad412ff66159043625f0e46",
+    "url": "/restaurantix/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/restaurantix/robots.txt"
+  }
+];
